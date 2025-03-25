0ec5f8a236b0b1a8ea2e206ecaf7d5512194cfb9
SIDDU2402 <151457448+SIDDU2402@users.noreply.github.com>
2025-03-25 12:25:23 +0530

    Add files via upload
    Roting

---
+++ Route/react/src/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+      <div>
+        <h2>About Page</h2>
+        <p>This is the about page of the application.</p>
+      </div>
+    )
+  }
+  
+  export default About
